d70832f904ba93013c0ebe7eaeb339a4dd43132d
krc28 <restreka28@gmail.com>
2021-09-23 17:14:12 -0500

    listo

--- DESARROLLO SOFTWARE/TEORIA HTML.bat
+++ DESARROLLO SOFTWARE/TEORIA HTML.bat
@@ -80,7 +80,94 @@ etiqueta <meta>.
 • Se pueden especificar los atributos name (tipo de
 información) y content (valor de dicha información).
 • Los tipos de información más utilizados son:
+Tipo                         Significado
+author                       Autor de la página
+classification               Palabras para clasificar la página en los buscadores
+description                  Descripción del contenido de la página
+generator                    Programa utilizado para crear la página
+keywords                     Palabras clave
+
 • No necesita etiqueta de cierre.
 • También se utiliza para indicarle al navegador alguna
 información o alguna acción que debe realizar. En este caso
 se utiliza el atributo http-equiv.
+
+"Etiqueta <body> </body>
+• El cuerpo del documento contiene la información propia
+del documento, es decir, lo que queremos que se visualice,
+el texto de la página, las imágenes, los formularios, etc.
+
+"Etiqueta <p> </p>
+• Introducir texto en la página.
+• Varios espacios en blanco: &nbsp
+• Salto de línea: <br>
+
+"Enlaces <a> </a>
+• Un hiperenlace, hipervínculo o vínculo no es más que un
+enlace, que al ser pulsado lleva a una página o archivo.
+• Aquellos elementos (texto, imágenes, etc.) sobre los que
+se desee insertar un enlace han de encontrarse entre las
+etiquetas <a> y </a>.
+• A través del atributo href se especifica la página o el archivo
+a la que está asociado el enlace.
+• Referencia absoluta
+• Referencia relativa al sitio:
+• <a href=“/nombreDelArchivo.html”>Ir a … </a>
+• Punto de fijación:
+• <a href=“/nombreDocumento.html #nombrePunto“> Ir a
+punto </a>
+• _blank:
+• Abre el documento vinculado en una ventana nueva del
+navegador.
+• _self:
+• Es la opción predeterminada. Abre el documento
+vinculado en el mismo marco o ventana que el vínculo
+• link permite determinar el color de los enlaces sin visitar
+(enlace que no ha sido pulsado ninguna vez).
+• active permite determinar el color del enlace activo (enlace
+que acaba de ser pulsado).
+• visited permite determinar el color de los enlaces visitados
+(enlaces que ya han sido pulsados).
+
+"Tablas <table> </table>
+• Las tablas están formadas por celdas, que son los
+recuadros que se obtienen como resultado de la
+intersección entre una fila y una columna
+• Etiquetas
+• <table></table>
+• <tr></tr>
+• <td></td>
+
+"Atributos de una tabla
+• align: alinea horizontalmente la tabla con respecto a su
+entorno.
+• background: permite poner un fondo para la tabla a partir
+de un enlace a una imagen.
+• bgcolor: da color de fondo a la tabla.
+• border: define el número de pixeles del borde principal.
+• bordercolor: define el color del borde.
+• cellpadding: define, en pixeles, el espacio entre los bordes
+de la celda y el contenido de la misma.
+• cellspacing: define el espacio entre los bordes (en pixeles).
+• height: define la altura de la tabla en pixeles o porcentaje.
+• width: define la anchura de la tabla en pixeles o porcentaje
+
+"Atributos de una celda
+• bgcolor =”nombre del color / número hexadecimal”.
+Define el color de la celda o fila.
+• bordercolor=”nombre del color / número hexadecimal”.
+Define el color del borde de la celda.
+• align=”top / middle / bottom”. Elegimos el lugar en el cual
+el texto es colocado. Top (arriba), middle (centro) y bottom
+(abajo).
+• valign=”left / center / right”. Alineación del texto
+izquierda (left), centro (center) o derecha (right)
+• colspan=”n”. Expande una celda horizontalmente tantas
+celdas como el valor asignado a n.
+• rowspan=”n”. Expande una celda verticalmente tantas
+celdas como el valor asignado a n.
+• width=”n”. El valor n indica la anchura de la tabla en
+pixeles (200) o porcentaje (20%). Este atributo también se
+puede aplicar a las celdas <td>.
+• height =”n”. El valor n define la altura de la celda en pixeles
+o porcentaje.
